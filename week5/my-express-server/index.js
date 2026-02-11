41dab4aff9fc88eba79979c78a7ec5f70e635e3b
Coralia Montalvo <corita@Coralias-Air-2.lan>
2026-02-10 21:11:44 -1000

    Add new files and updates for Spring 2026 assignments

---
+++ week5/my-express-server/index.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+console.log("Hello CodeSandbox");
+//jshint esversion:6
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>
+  res.send(
+    "Hello World! This is the main page - URL displayed by Code Sandbox!"
+  )
+);
+
+app.get("/contact", (req, res) => res.send("This is the contact page!"));
+
+app.get("/news", (req, res) => res.send("This is the news page!"));
+
+app.get("/about", function (req, res) {
+  res.send("This is a the bio page!");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
